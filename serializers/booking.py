0704ad1f04de0437d4b61fa2397d9ab0327bdc3c
LeonidKanin <122519875+LeonidKanin@users.noreply.github.com>
2023-05-19 19:12:35 +0700

    Add files via upload

---
+++ serializers/booking.py
@@ -0,0 +1,39 @@
+# Подготовка тестовых классов Pydantic для проверки структур данных запросов и ответов
+
+from pydantic import BaseModel, Field
+
+
+class AuthBody(BaseModel):
+    username: str = Field(...)
+    password: str = Field(...)
+
+class AuthResponse(BaseModel):
+    token: str = Field(...)
+
+
+class BookingDates(BaseModel):
+    checkin: str = Field(...)
+    checkout: str = Field(...)
+
+
+class Booking(BaseModel):
+    firstname: str = Field(...)
+    lastname: str = Field(...)
+    totalprice: int = Field(...)
+    depositpaid: bool = Field(...)
+    bookingdates: BookingDates = Field(...)
+    additionalneeds: str = Field(...)
+
+
+class BookingReduced(BaseModel):
+    firstname: str = Field(...)
+    lastname: str = Field(...)
+    totalprice: int = Field(...)
+    depositpaid: bool = Field(...)
+    bookingdates: BookingDates = Field(...)
+    additionalneeds: str = Field(None)
+
+
+class BookingIdsResponse(BaseModel):
+    bookingid: int = Field(...)
+
